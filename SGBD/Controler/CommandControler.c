6ae37aad089fd15e137f3daef9734357ecdba316
Clad62 <louis62400@gmail.com>
2015-11-22 19:35:49 +0100

    ajout de cas d'erreurs

--- SGBD/Controler/CommandControler.c
+++ SGBD/Controler/CommandControler.c
@@ -10,6 +10,18 @@
 #include "Database.h"
 #include "CommandControler.h"
 
+#define MAX_ADD_TABLE 3
+#define MAX_ADD_COL 5
+#define MAX_ADD_TUPLE 4
+
+#define MAX_DEL_TABLE 3
+#define MAX_DEL_COL 3
+
+#define MAX_DISP_TABLE 2
+#define MAX_DISP_COL 3
+#define MAX_DISP_TUPLE 3
+
+#define MAX_QUIT 1
 
 typedef struct
 {
@@ -97,53 +109,228 @@ static TabMot* cutStr(char* str)
 
 void commandManager(Database *db, char *command, DisplayFunc fct)
 {
-	char *substr;
+	TabMot *substr;
 
 	if (db == NULL || command == NULL)
 		return;
 
 	// On récupère les trois premiers caractères
-	substr = getStrBeforeCarac(command, ' ');
+	substr = cutStr(command);
 
-	if (substr == NULL)
+	if (substr->nbMot == 0)
 	{
-		char* error = "%s%s%s", buffer;
+		char* error = "%s%s", buffer;
 
-		buffer = (char*)malloc(sizeof(char) * (strlen(ERROR_HEADER) + strlen(UNKNOWN_KEYWORD) + strlen(command) + 1) );
+		buffer = (char*)malloc(sizeof(char) * (strlen(ERROR_HEADER) + strlen(NOT_ENOUGH_WORDS) + 1) );
 
-		sprintf(buffer, error, ERROR_HEADER, UNKNOWN_KEYWORD, command);
+		sprintf(buffer, error, ERROR_HEADER, NOT_ENOUGH_WORDS);
 		fct(buffer);
 
 		free(buffer);
 		return;
 	}
 
-	toUpperCase(substr);
+	toUpperCase(substr->tab[0]);
 
-	if (! strcmp(substr, ADD_KEYWORD))
+	if (! strcmp(substr->tab[0], ADD_KEYWORD))
 	{
+		if (! strcmp(substr->tab[1],TABLE_KEYWORD))
+		{
+			if (substr->nbMot > MAX_ADD_TABLE)
+			{
+				char* error = "%s%s", buffer;
 
+				buffer = (char*)malloc(sizeof(char) * (strlen(ERROR_HEADER) + strlen(TOO_MUCH_PARAM) + 1) );
+
+				sprintf(buffer, error, ERROR_HEADER, TOO_MUCH_PARAM);
+				fct(buffer);
+				free(buffer);
+			}
+			else if (substr->nbMot < MAX_ADD_TABLE)
+			{
+				char* error = "%s%s", buffer;
+
+				buffer = (char*)malloc(sizeof(char) * (strlen(ERROR_HEADER) + strlen(NOT_ENOUGH_PARAM) + 1) );
+
+				sprintf(buffer, error, ERROR_HEADER, NOT_ENOUGH_PARAM);
+				fct(buffer);
+				free(buffer);
+			}
+			else
+			{
+
+			}
+		}
+		else if (! strcmp(substr->tab[1],COL_KEYWORD))
+		{
+			if (substr->nbMot > MAX_ADD_COL)
+			{
+				char* error = "%s%s", buffer;
+
+				buffer = (char*)malloc(sizeof(char) * (strlen(ERROR_HEADER) + strlen(TOO_MUCH_PARAM) + 1) );
+
+				sprintf(buffer, error, ERROR_HEADER, TOO_MUCH_PARAM);
+				fct(buffer);
+				free(buffer);
+			}
+			else if (substr->nbMot < MAX_ADD_COL)
+			{
+				char* error = "%s%s", buffer;
+
+				buffer = (char*)malloc(sizeof(char) * (strlen(ERROR_HEADER) + strlen(NOT_ENOUGH_PARAM) + 1) );
+
+				sprintf(buffer, error, ERROR_HEADER, NOT_ENOUGH_PARAM);
+				fct(buffer);
+				free(buffer);
+			}
+			else
+			{
+
+			}
+		}
+		else if (! strcmp(substr->tab[1],TUPLE_KEYWORD))
+		{
+			if (substr->nbMot < MAX_ADD_TUPLE)
+			{
+				char* error = "%s%s", buffer;
+
+				buffer = (char*)malloc(sizeof(char) * (strlen(ERROR_HEADER) + strlen(NOT_ENOUGH_PARAM) + 1) );
+
+				sprintf(buffer, error, ERROR_HEADER, NOT_ENOUGH_PARAM);
+				fct(buffer);
+				free(buffer);
+			}
+			else
+			{
+
+			}
+		}
 	}
-	else if (! strcmp(substr, DELETE_KEYWORD) )
+	else if (! strcmp(substr->tab[0], DELETE_KEYWORD) )
 	{
+		if (! strcmp(substr->tab[1],TABLE_KEYWORD))
+		{
+			if (substr->nbMot > MAX_DEL_TABLE)
+			{
+				char* error = "%s%s", buffer;
 
+				buffer = (char*)malloc(sizeof(char) * (strlen(ERROR_HEADER) + strlen(TOO_MUCH_PARAM) + 1) );
+
+				sprintf(buffer, error, ERROR_HEADER, TOO_MUCH_PARAM);
+				fct(buffer);
+				free(buffer);
+			}
+			else if (substr->nbMot < MAX_DEL_TABLE)
+			{
+				char* error = "%s%s", buffer;
+
+				buffer = (char*)malloc(sizeof(char) * (strlen(ERROR_HEADER) + strlen(NOT_ENOUGH_PARAM) + 1) );
+
+				sprintf(buffer, error, ERROR_HEADER, NOT_ENOUGH_PARAM);
+				fct(buffer);
+				free(buffer);
+			}
+			else
+			{
+
+			}
+		}
+		else if (! strcmp(substr->tab[1],COL_KEYWORD))
+		{
+			if (substr->nbMot > MAX_DEL_COL)
+			{
+				char* error = "%s%s", buffer;
+
+				buffer = (char*)malloc(sizeof(char) * (strlen(ERROR_HEADER) + strlen(TOO_MUCH_PARAM) + 1) );
+
+				sprintf(buffer, error, ERROR_HEADER, TOO_MUCH_PARAM);
+				fct(buffer);
+				free(buffer);
+			}
+			else if (substr->nbMot < MAX_DEL_COL)
+			{
+				char* error = "%s%s", buffer;
+
+				buffer = (char*)malloc(sizeof(char) * (strlen(ERROR_HEADER) + strlen(NOT_ENOUGH_PARAM) + 1) );
+
+				sprintf(buffer, error, ERROR_HEADER, NOT_ENOUGH_PARAM);
+				fct(buffer);
+				free(buffer);
+			}
+			else
+			{
+
+			}
+		}
 	}
 	// Si le début ne correspond pas à un ajout ou une suppression, on vérifie s'il s'agit d'une requête d'affichage, ou de quitter le programme
-	else if (! strcmp(substr, DISPLAY_KEYWORD) )
+	else if (! strcmp(substr->tab[0], DISPLAY_KEYWORD) )
 	{
+		if (! strcmp(substr->tab[1],TABLE_KEYWORD))
+		{
+			if (substr->nbMot > MAX_DISP_TABLE)
+			{
+				char* error = "%s%s", buffer;
 
+				buffer = (char*)malloc(sizeof(char) * (strlen(ERROR_HEADER) + strlen(TOO_MUCH_PARAM) + 1) );
+
+				sprintf(buffer, error, ERROR_HEADER, TOO_MUCH_PARAM);
+				fct(buffer);
+				free(buffer);
+			}
+			else if (substr->nbMot < MAX_DISP_TABLE)
+			{
+				char* error = "%s%s", buffer;
+
+				buffer = (char*)malloc(sizeof(char) * (strlen(ERROR_HEADER) + strlen(NOT_ENOUGH_PARAM) + 1) );
+
+				sprintf(buffer, error, ERROR_HEADER, NOT_ENOUGH_PARAM);
+				fct(buffer);
+				free(buffer);
+			}
+			else {
+
+			}
+		}
+		else if (! strcmp(substr->tab[1],COL_KEYWORD))
+		{
+			if (substr->nbMot > MAX_DISP_COL)
+				;
+			else if (substr->nbMot < MAX_DISP_COL)
+				;
+		}
+		else if (! strcmp(substr->tab[1],TUPLE_KEYWORD))
+		{
+			if (substr->nbMot > MAX_DISP_TUPLE)
+				;
+			else if (substr->nbMot < MAX_DISP_TUPLE)
+				;
+		}
 	}
-	else if (! strcmp(substr, QUIT_KEYWORD) )
+	else if (! strcmp(substr->tab[0], QUIT_KEYWORD) )
 	{
+		if (substr->nbMot > MAX_QUIT)
+		{
+			char* error = "%s%s", buffer;
 
+			buffer = (char*)malloc(sizeof(char) * (strlen(ERROR_HEADER) + strlen(TOO_MUCH_PARAM) + 1) );
+
+			sprintf(buffer, error, ERROR_HEADER, TOO_MUCH_PARAM);
+			fct(buffer);
+			free(buffer);
+		}
+		else
+		{
+
+		}
 	}
 	else
 	{
 		char* error = "%s%s%s", buffer;
 
-		buffer = (char*)malloc(sizeof(char) * (strlen(ERROR_HEADER) + strlen(UNKNOWN_KEYWORD) + strlen(substr) + 1) );
+		buffer = (char*)malloc(sizeof(char) * (strlen(ERROR_HEADER) + strlen(UNKNOWN_KEYWORD) + strlen(substr->tab[0]) + 1) );
 
-		sprintf(buffer, error, ERROR_HEADER, UNKNOWN_KEYWORD, substr);
+		sprintf(buffer, error, ERROR_HEADER, UNKNOWN_KEYWORD, substr->tab[0]);
 		fct(buffer);
 		free(buffer);
 	}
